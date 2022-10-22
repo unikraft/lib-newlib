unikraft/lib-newlib

commit 58f9f3a3f7b5ca9c5f0c7c095e067abba29f5ed6

include/fcntl.h: Replace O_NONBLOCK with new one on arm
in newlib _default_fcntl.h, it defines O_NONBLOCK as follows:
\#define _FNONBLOCK      0x4000  /* non blocking I/O (POSIX style) */
But it is different from linux definition(04000=0x800):
\#define O_NONBLOCK	00004000

This difference confused virtio-9p open() syscall. If you open a
file with O_NONBLOCK(is 0x4000 in newlib) and sys_open will regard
it as O_DIRECTORY. At last it returns a ENOTDIR error.

Signed-off-by: Jia He <justin.he@arm.com>
Reviewed-by: Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>

include/fcntl.h
@@ -15,6 +15,8 @@
 #define O_DIRECT     040000
 #define O_NOATIME  01000000
 #elif ((defined CONFIG_ARCH_ARM_64) || (defined CONFIG_ARCH_ARM_32))
+#undef O_NONBLOCK
+#define O_NONBLOCK    04000
 #define O_NOFOLLOW  0100000
 #define O_DIRECTORY  040000
 #define O_CLOEXEC  02000000

commit 534cea53762f718a2cd93fc2160364c76f6f3b39

Define O_DIRECTORY with different value
Previous fix regarding confusion between O_NONBLOCK and O_DIRECTORY
values changed O_NONBLOCK to a different number, therefore it drifted
away from FNONBLOCK (for which it was an alias) and caused a blocking
socket.

This commit simply changes the value of O_DIRECTORY, without being
necessary to redefine O_NONBLOCK or FNONBLOCK.

Fixes: 58f9f3a3f7b5 ("include/fcntl.h: Replace O_NONBLOCK with new one
on arm")

Signed-off-by: Maria Sfiraiala <maria.sfiraiala@gmail.com>

include/fcntl.h
@@ -15,10 +15,8 @@
 #define O_DIRECT     040000
 #define O_NOATIME  01000000
 #elif ((defined CONFIG_ARCH_ARM_64) || (defined CONFIG_ARCH_ARM_32))
-#undef O_NONBLOCK
-#define O_NONBLOCK    04000
 #define O_NOFOLLOW  0100000
-#define O_DIRECTORY  040000
+#define O_DIRECTORY 0400000000
 #define O_CLOEXEC  02000000
 #define O_DSYNC      010000
 #define O_ASYNC      020000